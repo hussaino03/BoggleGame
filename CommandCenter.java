18a083c931e23975ba095119c03800db52c11b70
santonip <sebastian.santoni@mail.utoronto.ca>
2022-11-24 12:55:32 -0500

    [Sebastian-1.1, 1.2] Created all classes following the Command Pattern. Finalizing the implementation.

--- CommandCenter.java
+++ CommandCenter.java
@@ -1,19 +1,59 @@
-import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.scene.Scene;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class CommandCenter implements EventHandler<ActionEvent> {
+
+    // This class serves as the invoker for the command pattern
+    Stage stage;
+    Queue<Command> commandQueue;
+
+    // Information required for RedirectScreenCommand
+    Scene transitionScene;
+
+    String transitionTitle;
+
+    // Information required for ProcessInformationCommand
+
+    String choice;
+
+
+    // Constructor which takes in the information from the command for RedirectScreen
+
+    // Maybe we can overload the constructor with different parameters
+    // For example, one constructor only takes in the info for RedirectScreen so that
+    // unnecessary information is not passed in
+    public CommandCenter(Stage newStage, Scene newScene, String title, String info) {
+        this.stage = newStage;
+        this.transitionScene = newScene;
+        this.transitionTitle = title;
+        this.choice = info;
+        commandQueue = new PriorityQueue<Command>();
+    }
+
+    private void setCommand(Command command) {
+        commandQueue.add(command);
+    }
+
+    private void execute() {
+        for (Command c : this.commandQueue) {
+            c.execute();
+        }
+    }
+    // handle method which processes commands
     @Override
     public void handle(ActionEvent actionEvent) {
         Node event = (Node) actionEvent.getSource();
-        if (event.getId().equals("How to Play")) {
-            System.out.println("Here are the rules");
+        if (event.getId().equals("How to Play") || event.getId().equals("Return to Main Menu")) {
+            this.setCommand(new RedirectScreenCommand(stage, transitionScene, transitionTitle));
         }
+        this.execute();
         }
+
     }
 
--- MainStartScreen.java
+++ MainStartScreen.java
@@ -1,10 +1,13 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.Scene;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 
 
 public class MainStartScreen extends Application {
@@ -15,14 +18,77 @@ public class MainStartScreen extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("Boggle");
-        Button button = new Button("How to Play");
-        button.setId("How to Play");
-        button.setOnAction(new CommandCenter());
-        StackPane layout =  new StackPane();
-        layout.getChildren().add(button);
-        Scene scene = new Scene(layout, 300, 250);
-        stage.setScene(scene);
+        stage.setTitle("Main Menu"); // Stage setup
+
+        // Four different buttons the user can select
+        Button howToPlayButton = new Button("How to Play");
+        howToPlayButton.setId("How to Play");
+
+        Button statsButton = new Button("Stats");
+        statsButton.setId("Stats");
+
+        Button normalModeButton = new Button("Normal Mode");
+        normalModeButton.setId("Normal Mode");
+
+        Button timedModeButton = new Button("Timed Mode");
+        timedModeButton.setId("Timed Mode");
+
+        // Setup for the main scene and layout [Contains the four buttons]
+        GridPane mainLayout =  new GridPane();
+        mainLayout.setPadding(new Insets(10,10,10,10));
+        mainLayout.setVgap(20);
+        mainLayout.setHgap(20);
+        Scene mainScene = new Scene(mainLayout, 700, 700);
+
+        // add buttons to main layout
+        mainLayout.add(normalModeButton, 10, 10);
+        mainLayout.add(timedModeButton, 11, 10);
+        mainLayout.add(howToPlayButton, 10, 11);
+        mainLayout.add(statsButton, 11, 11);
+
+        // Setup for How to Play scene and layout
+        GridPane howToPlayLayout =  new GridPane();
+        howToPlayLayout.setPadding(new Insets(10,10,10,10));
+        howToPlayLayout.setVgap(20);
+        howToPlayLayout.setHgap(20);
+        Scene howToPlayScene = new Scene(howToPlayLayout, 700, 700);
+
+        // Add text and button to how to play layout
+        Label gameInstructions = new Label(" Boggle is a game where you try to find as" +
+                " many words as you can in a randomized grid of letters. \n Words can be" +
+                " formed by joining letters which are vertically, horizontally or diagonally" +
+                " adjacent. \n You will earn points based on the length of the word found. \n " +
+                "You can choose between two game modes: Normal and Timed. \n In the Normal Game Mode, " +
+                "you will play against the computer. \n You will be given a grid of letters and should" +
+                " form as many words as you can. \n When you can no longer think of any words, the " +
+                "computer will find all remaining words. \n At the end of each round, you will be given" +
+                " information regarding your current standing against the computer. \n Your goal is to" +
+                " end the game with a higher score than the computer. \n In the Timed Game Mode, you" +
+                " will play against yourself. \n You will be given a grid of letters and should form" +
+                " as many words as you can before time runs out. \n Each word you find will extend your " +
+                "time proportional to the length of the word found. \n Your goal is to beat your " +
+                "previous high score. \n You can return to this page at any time to review the " +
+                "game instructions. \n You can also go to the Stats Page to review your success across" +
+                " all games.");
+
+        Button goBackButton = new Button("Return to Main Menu");
+        goBackButton.setId("Return to Main Menu");
+        howToPlayLayout.add(gameInstructions, 0, 0);
+        howToPlayLayout.add(goBackButton, 0, 1);
+
+        // Send all button clicks to commandCenter for these commands to be handled
+        CommandCenter htpCommandCenter = new CommandCenter(stage, howToPlayScene, "How to Play",
+                "");
+        CommandCenter mainMenuCommandCenter = new CommandCenter(stage, mainScene, "Main Menu",
+                "");
+        howToPlayButton.setOnAction(htpCommandCenter);
+//        statsButton.setOnAction(commandCenter);
+//        normalModeButton.setOnAction(commandCenter);
+//        timedModeButton.setOnAction(commandCenter);
+        goBackButton.setOnAction(mainMenuCommandCenter);
+
+        // Set the scene to the main scene when first running the game
+        stage.setScene(mainScene);
         stage.show();
     }
 }
