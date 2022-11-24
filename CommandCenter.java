8c96510698c98fd84bbb097396246093799768e1
santonip <sebastian.santoni@mail.utoronto.ca>
2022-11-24 14:26:15 -0500

    [Sebastian-1.1, 1.2] Finished both user stories

--- CommandCenter.java
+++ CommandCenter.java
@@ -4,36 +4,19 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
 
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class CommandCenter implements EventHandler<ActionEvent> {
 
     // This class serves as the invoker for the command pattern
-    Stage stage;
-    Queue<Command> commandQueue;
+    private gameWindow gameWindow;
+    private Queue<Command> commandQueue;
 
-    // Information required for RedirectScreenCommand
-    Scene transitionScene;
-
-    String transitionTitle;
-
-    // Information required for ProcessInformationCommand
-
-    String choice;
-
-
-    // Constructor which takes in the information from the command for RedirectScreen
-
-    // Maybe we can overload the constructor with different parameters
-    // For example, one constructor only takes in the info for RedirectScreen so that
-    // unnecessary information is not passed in
-    public CommandCenter(Stage newStage, Scene newScene, String title, String info) {
-        this.stage = newStage;
-        this.transitionScene = newScene;
-        this.transitionTitle = title;
-        this.choice = info;
-        commandQueue = new PriorityQueue<Command>();
+    public CommandCenter(gameWindow g) {
+        this.gameWindow = g;
+        commandQueue = new LinkedList<Command>();
     }
 
     private void setCommand(Command command) {
@@ -49,9 +32,12 @@ public class CommandCenter implements EventHandler<ActionEvent> {
     @Override
     public void handle(ActionEvent actionEvent) {
         Node event = (Node) actionEvent.getSource();
-        if (event.getId().equals("How to Play") || event.getId().equals("Return to Main Menu")) {
-            this.setCommand(new RedirectScreenCommand(stage, transitionScene, transitionTitle));
-        }
+        String newScene = event.getId().split(", ")[1];
+        System.out.println(newScene);
+        Stage stage = gameWindow.primaryStage;
+        Scene transition = gameWindow.scenes.get(newScene);
+        String title = gameWindow.sceneTitles.get(transition);
+        this.setCommand(new RedirectScreenCommand(stage, transition, title));
         this.execute();
         }
 
--- gameWindow.java
+++ gameWindow.java
@@ -1,37 +1,39 @@
 import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.scene.Scene;
+import java.util.HashMap;
 
 
-public class MainStartScreen extends Application {
+public class gameWindow extends Application {
 
+    HashMap<String, Scene> scenes = new HashMap<String, Scene>();
+    HashMap<Scene, String> sceneTitles = new HashMap<Scene, String>();
+    Stage primaryStage;
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
+        this.primaryStage = stage;
         stage.setTitle("Main Menu"); // Stage setup
 
         // Four different buttons the user can select
         Button howToPlayButton = new Button("How to Play");
-        howToPlayButton.setId("How to Play");
+        howToPlayButton.setId("How to Play, How To Play Scene");
 
         Button statsButton = new Button("Stats");
-        statsButton.setId("Stats");
+        statsButton.setId("Stats, Stats Scene");
 
         Button normalModeButton = new Button("Normal Mode");
-        normalModeButton.setId("Normal Mode");
+        normalModeButton.setId("Normal Mode, Grid Selection Scene");
 
         Button timedModeButton = new Button("Timed Mode");
-        timedModeButton.setId("Timed Mode");
+        timedModeButton.setId("Timed Mode, Grid Selection Scene");
 
         // Setup for the main scene and layout [Contains the four buttons]
         GridPane mainLayout =  new GridPane();
@@ -39,8 +41,12 @@ public class MainStartScreen extends Application {
         mainLayout.setVgap(20);
         mainLayout.setHgap(20);
         Scene mainScene = new Scene(mainLayout, 700, 700);
+        scenes.put("Main Scene", mainScene);
+        sceneTitles.put(mainScene, "Main Menu");
 
         // add buttons to main layout
+        // buttonIds are used to encapsulate commands via the following ID format:
+        // "ID, Scene to transition to"
         mainLayout.add(normalModeButton, 10, 10);
         mainLayout.add(timedModeButton, 11, 10);
         mainLayout.add(howToPlayButton, 10, 11);
@@ -52,6 +58,8 @@ public class MainStartScreen extends Application {
         howToPlayLayout.setVgap(20);
         howToPlayLayout.setHgap(20);
         Scene howToPlayScene = new Scene(howToPlayLayout, 700, 700);
+        scenes.put("How To Play Scene", howToPlayScene);
+        sceneTitles.put(howToPlayScene, "How to Play");
 
         // Add text and button to how to play layout
         Label gameInstructions = new Label(" Boggle is a game where you try to find as" +
@@ -71,21 +79,48 @@ public class MainStartScreen extends Application {
                 "game instructions. \n You can also go to the Stats Page to review your success across" +
                 " all games.");
 
-        Button goBackButton = new Button("Return to Main Menu");
-        goBackButton.setId("Return to Main Menu");
+        Button goBackFromHTPButton = new Button("Return to Main Menu");
+        goBackFromHTPButton.setId("Return to Main Menu, Main Scene");
         howToPlayLayout.add(gameInstructions, 0, 0);
-        howToPlayLayout.add(goBackButton, 0, 1);
+        howToPlayLayout.add(goBackFromHTPButton, 0, 1);
+
+        // Setup for stats scene and layout
+        GridPane statsLayout =  new GridPane();
+        statsLayout.setPadding(new Insets(10,10,10,10));
+        statsLayout.setVgap(20);
+        statsLayout.setHgap(20);
+        Scene statsScene = new Scene(statsLayout, 700, 700);
+        scenes.put("Stats Scene", statsScene);
+        sceneTitles.put(statsScene, "Stats");
+
+        // Add button to stats layout
+        Button goBackFromStatsButton = new Button("Return to Main Menu");
+        goBackFromStatsButton.setId("Return to Main Menu, Main Scene");
+        statsLayout.add(goBackFromStatsButton, 5, 5);
+
+        // Setup for grid selection scene and layout
+        GridPane gridSelectionLayout =  new GridPane();
+        gridSelectionLayout.setPadding(new Insets(10,10,10,10));
+        gridSelectionLayout.setVgap(20);
+        gridSelectionLayout.setHgap(20);
+        Scene gridSelectionScene = new Scene(gridSelectionLayout, 700, 700);
+        scenes.put("Grid Selection Scene", gridSelectionScene);
+        sceneTitles.put(gridSelectionScene, "Grid Selection");
+
+        // add button to grid selection layout
+        Button goBackFromGridSelectionButton = new Button("Return to Main Menu");
+        goBackFromGridSelectionButton.setId("Return to Main Menu, Main Scene");
+        gridSelectionLayout.add(goBackFromGridSelectionButton, 5, 5);
 
         // Send all button clicks to commandCenter for these commands to be handled
-        CommandCenter htpCommandCenter = new CommandCenter(stage, howToPlayScene, "How to Play",
-                "");
-        CommandCenter mainMenuCommandCenter = new CommandCenter(stage, mainScene, "Main Menu",
-                "");
-        howToPlayButton.setOnAction(htpCommandCenter);
-//        statsButton.setOnAction(commandCenter);
-//        normalModeButton.setOnAction(commandCenter);
-//        timedModeButton.setOnAction(commandCenter);
-        goBackButton.setOnAction(mainMenuCommandCenter);
+        CommandCenter commandCenter = new CommandCenter(this);
+        howToPlayButton.setOnAction(commandCenter);
+        statsButton.setOnAction(commandCenter);
+        normalModeButton.setOnAction(commandCenter);
+        timedModeButton.setOnAction(commandCenter);
+        goBackFromHTPButton.setOnAction(commandCenter);
+        goBackFromStatsButton.setOnAction(commandCenter);
+        goBackFromGridSelectionButton.setOnAction(commandCenter);
 
         // Set the scene to the main scene when first running the game
         stage.setScene(mainScene);
