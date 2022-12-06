e4f237ee5b304696baa21ca5c683a64a50c71685
santonip <sebastian.santoni@mail.utoronto.ca>
2022-12-06 14:42:20 -0500

    [Sebastian - Refactoring] Removed redundant access modifiers in the Command Interface.

--- command/Command.java
+++ command/Command.java
@@ -2,11 +2,11 @@ package command;
 
 /**
  * The Command Interface forces the classes which implement the interface to override the execute() method to ensure
- * all such classes can polymorphically execute via the execute() method
+ * all such classes can polymorphically executed via the execute() method
  */
 public interface Command {
     /**
      * Abstract method to be implemented by classes which implement the Command Interface
      */
-    public abstract void execute();
+    void execute();
 }
