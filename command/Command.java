2c2d2876da5ccfda770c0c5e565834980bbf813f
santonip <sebastian.santoni@mail.utoronto.ca>
2022-12-06 11:54:33 -0500

    [Sebastian - Refactoring] Updated documentation of Command Interface

--- command/Command.java
+++ command/Command.java
@@ -1,5 +1,12 @@
 package command;
 
+/**
+ * The Command Interface forces the classes which implement the interface to override the execute() method to ensure
+ * all such classes can polymorphically execute via the execute() method
+ */
 public interface Command {
+    /**
+     * Abstract method to be implemented by classes which implement the Command Interface
+     */
     public abstract void execute();
 }
