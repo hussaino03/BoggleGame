7a946ac28ea59efc8ad2f28241c6d0695e884e7d
santonip <sebastian.santoni@mail.utoronto.ca>
2022-12-06 12:38:19 -0500

    [Sebastian - Refactoring] Deleted unnecessary methods, print statements and commented out lines of code from BoggleGame.java

--- boggle/BoggleGame.java
+++ boggle/BoggleGame.java
@@ -62,26 +62,6 @@ public class BoggleGame {
         this.commandCenter = CommandCenter.getInstance(window);
     }
 
-    /*
-     * Provide instructions to the user, so they know how to play the game.
-     */
-
-    // This is obsolete because of the How To Play screen
-    public void giveInstructions()
-    {
-        System.out.println("The Boggle board contains a grid of letters that are randomly placed.");
-        System.out.println("We're both going to try to find words in this grid by joining the letters.");
-        System.out.println("You can form a word by connecting adjoining letters on the grid.");
-        System.out.println("Two letters adjoin if they are next to each other horizontally, ");
-        System.out.println("vertically, or diagonally. The words you find must be at least 4 letters long, ");
-        System.out.println("and you can't use a letter twice in any single word. Your points ");
-        System.out.println("will be based on word length: a 4-letter word is worth 1 point, 5-letter");
-        System.out.println("words earn 2 points, and so on. After you find as many words as you can,");
-        System.out.println("I will find all the remaining words.");
-        System.out.println("\nHit return when you're ready...");
-    }
-
-
     /**
      * Gets information from the user to initialize a new Boggle game.
      * It will loop until the user indicates they are done playing.
@@ -121,19 +101,16 @@ public class BoggleGame {
                 this.choiceProcessor.put("choice", "_");
             }
             //round is over! So, store the statistics, and end the round.
+
             //Shall we repeat?
-//            System.out.println("Play again? Type 'Y' or 'N'");
             if (this.choiceProcessor.get("choice").equals("N")) {
                 this.choiceProcessor.put("choice", "_");
                 break;
             }
-
-
         }
 
         //we are done with the game! So, summarize all the play that has transpired and exit.
         this.gameStats.summarizeGame();
-        System.out.println("Thanks for playing!");
     }
 
     /**
@@ -315,13 +292,9 @@ public class BoggleGame {
      * @param allWords A mutable list of all legal words that can be found, given the boggleGrid grid letters
      */
     private void humanMove(BoggleGrid board, Map<String,ArrayList<Position>> allWords){
-        System.out.println("It's your turn to find some words!");
-//        Scanner sc = new Scanner(System.in);
         Dictionary dict = new Dictionary("wordlist.txt");
         while(true) {
 
-//            System.out.println(board); Instead of printing this to the console, should be displayed
-//            System.out.println("Enter your word!");
             String word = this.choiceProcessor.get("Word");
 
             if (word.length() >= 4 && dict.containsWord(word) &&
@@ -331,11 +304,8 @@ public class BoggleGame {
                 gameStats.addWord(word.toLowerCase(), BoggleStats.Player.Human);
                 commandCenter.handle("DisplayInGameStats; ");
 
-            }else {
-                if (!word.equals("")) {
-//                    System.out.println("Invalid Word! Sorry!");
-                }
             }
+
             if (choiceProcessor.get("Round Ended").equals("true")){
                 commandCenter.handle("DisplayRoundStats; ");
                 this.choiceProcessor.put("Round Ended", "false");
