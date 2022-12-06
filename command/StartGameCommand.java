b136fa8f91bf78667c939ecad45277f6a3f9a276
santonip <sebastian.santoni@mail.utoronto.ca>
2022-12-06 14:21:58 -0500

    [Sebastian - Refactoring] Updated StartGameCommand JavaDoc

--- command/StartGameCommand.java
+++ command/StartGameCommand.java
@@ -2,13 +2,28 @@ package command;
 
 import boggle.BoggleGame;
 
+/**
+ * This command starts the game
+ */
+
 public class StartGameCommand implements Command {
+    /**
+     * The game which is to be started
+     */
     private final BoggleGame game;
 
+    /**
+     * StartGameCommand constructor
+     * @param g The game which is to be started
+     */
     public StartGameCommand(BoggleGame g) {
         this.game = g;
     }
 
+    /**
+     * Checks if a grid size has been selected and starts the game if so. Done on a separate thread
+     * so that playGame()'s infinite while loop and the GUI can run concurrently.
+     */
     public void execute() {
             Thread t = new Thread(()->{
                 if (game.gridSizeSelected()) {
@@ -16,6 +31,5 @@ public class StartGameCommand implements Command {
                 }
             });
             t.start();
-
     }
 }
